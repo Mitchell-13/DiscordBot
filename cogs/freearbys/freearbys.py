1c4bb81458246d9bfb5a8950961d8e610c9bf6b3
Mitchell Ogden <mitchellogden13@gmail.com>
2022-11-08 12:14:37 -0700

    clean up

--- cogs/freearbys/freearbys.py
+++ cogs/freearbys/freearbys.py
@@ -7,11 +7,9 @@ from dateutil import tz
 import asyncio
 import logging
 
-
 lastPlayed = None
 rand = random.randrange(0,5)
 MST = tz.gettz('Mountain Standard Time')
-
 
 class freearbys(commands.Cog):
     def __init__(self, client):
@@ -38,28 +36,38 @@ class freearbys(commands.Cog):
                 gameScore = f"Utah Jazz: {str(jazzScore)} | {opName}: {str(opScore)}"
                 return [jazzScore, gameScore]
 
+        def check_count():
+            count = 0
+            r = requests.get("https://balldontlie.io/api/v1/games?start_date=2022-10-19&team_ids[]=29")
+            j = r.json()
+            for games in j['data']:
+                if games['home_team']['full_name'] == "Utah Jazz":
+                    if games['home_team_score'] >= 111:
+                        count +=1
+                if games['visitor_team']['full_name'] == "Utah Jazz":
+                    if games['visitor_team_score'] >= 111:
+                        count+=1
+            return count
+
+
         @tasks.loop(time=datetime.time(12, 0, 0, 0, tzinfo=MST))
         async def free_food_message():
             time = datetime.strftime(datetime.now(), '%H:%M')
             global lastPlayed
             if (lastPlayed == None or abs((lastPlayed - datetime.now())) > timedelta(hours=2)) and time == "12:00":
                 l = check_game()
+                count = check_count()
                 if l is not None:
                     channel = client.get_channel(self.config['channel_gamers'])
                     if l[0] >= 111:
-                        await channel.send(f"{self.config['role_arbys']}\n{self.config['msg'][rand]}\n\nYesterday's game score: \n{l[1]}")
+                        await channel.send(f"{self.config['role_arbys']}\n{self.config['msg'][rand]}\n\nYesterday's game score: \n{l[1]}\n\nArby's won this season: {count}")
                     else:
-                        await channel.send(f"No free Arby's today :(\n\nYesterday's game score: \n{l[1]}")
+                        await channel.send(f"No free Arby's today :(\n\nYesterday's game score: \n{l[1]}\n\nArby's won this season: {count}")
                     lastPlayed = datetime.now()
 
-
-
         @commands.Cog.listener()
-        async def start(self, ctx: commands.Context):
-
+        async def on_ready(self, ctx: commands.Context):
             free_food_message.start()
     
-   
-
 async def setup(client: commands.Bot):
     await client.add_cog(freearbys(client))
