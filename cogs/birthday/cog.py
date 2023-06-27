b0c2753c9502efe57fcea12e29bb3f813caff7d2
Mitchell <mitchellogden13@gmail.com>
2023-06-27 12:00:18 -0600

    fix message

--- cogs/birthday/cog.py
+++ cogs/birthday/cog.py
@@ -15,7 +15,7 @@ class BirthdayCog(commands.Cog):
 
         if message.author.name == self.config['bday-user']:
             logging.debug(f'User {message.author.name} sent a message.')
-            await message.channel.send("Hey, the target user said something in the target channel!")
+            await message.channel.send("HAPPY 😊 BIRTHDAY 🎂 BRO!!! 😎😜😩😆 Its time 🕦 to get LIT 🔥 for your special 👉😎👈 BIRTHDAY 🎊! You finna turn UP 👆🤯 and get DOWN 👇🤩 on this BEAUTIFUL day 😩💕 because you're 👀 a CHAMP🎖and a COOL 😎 DUDE 💪!\nD**k the Birthday Boy\nBirthday dog!")
 
 async def setup(client: commands.Bot):
     await client.add_cog(BirthdayCog(client))
